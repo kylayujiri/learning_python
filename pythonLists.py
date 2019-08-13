d59bdfbc72a0977057eb6ff11dab38d29325c453
Kyla Yujiri <27860923+kylayujiri@users.noreply.github.com>
2019-08-13 10:42:22 -0700

    Added to pythonOverview and created pythonLists.py

---
+++ pythonLists.py
@@ -0,0 +1,76 @@
+# LISTS
+
+# A list is an ordered set of objects
+list1 = [10, 20, 30, 40]
+list2 = ["A String", 1]
+
+# A list of lists
+my_list_of_lists = [['Hi', 5], ['Hello',2]]
+
+# Zip Function - returns an object that contains a list of pairs
+# combines the two lists so element 1 of list 1 is paired with element 1 of list 2
+# only pairs items until it reaches the length of the shorter list
+myObject = zip(list1,list2)
+print(list(myObject))
+
+# Empty List
+my_empty_list = []
+
+# Append - adds a new element to the end of a list
+my_list = [1, 2]
+my_list.append(3)
+print(my_list)
+
+# Combining Lists
+old_list = [1, 2, 3]
+new_list = old_list + [4, 5]
+print(new_list)
+
+# Range Function - returns an object with the numbers 0 - (n-1)
+my_range = range(5) # 0, 1, 2, 3, 4
+my_range2 = range(2, 6) # 2, 3, 4, 5
+my_range3 = range(1, 50, 10) # 1, 11, 21, 31, 41
+
+print(list(my_range))
+
+# Length of a List
+list_length = len(list1)
+
+# Referencing an element of a list
+num = list1[0]
+
+# Referencing the last element of a list
+last_num = list1[-1]
+
+# Slicing
+new_list = list1[1:3] # 20, 30
+new_list2 = list1[:2] # starts at index 0; thus will result in 10, 20
+new_list3 = list1[2:] # ends at the end of the list; 30, 40
+new_list4 = list1[-3:] # gives the last 3 elements; 20, 30, 40
+print(new_list, new_list2, new_list3, new_list4)
+
+# counting matching elements
+my_name = ['k', 'y', 'l', 'a', 'y', 'u', 'j', 'i', 'r', 'i']
+num_y = my_name.count('y')
+num_i = my_name.count('i')
+
+# in place sort
+list_to_sort = [10, 5, 6, 2, 3]
+list_to_sort.sort() # does not return anything
+print(list_to_sort)
+
+# another way to sort
+list_to_sort2 = [20, 30, 10, 0] # this list will remain unaffected
+sorted_list = sorted(list_to_sort2) # returns the sorted list
+print(list_to_sort2, sorted_list)
+
+# pop - takes the last item off a list and returns in
+list_to_pop = [1, 2, 3]
+last_item = list_to_pop.pop()
+print(last_item)
+
+# finding the index
+list_to_search = [1, 2, 3, 4, 5]
+index_of_3 = list_to_search.index(3)
+print(index_of_3)
+# if you give a value that is not in the list, there will be an error (NameError)
--- pythonOverview.py
+++ pythonOverview.py
@@ -31,6 +31,16 @@ my_MLS = """ Hi!
 This is my Multi Line String (MLS)."""
 
 # Lists
+my_list = [0, 1, 2, 3, 4, 5]
+# for more on lists see pythonLists.py
+
+# Tuples - immutable data structure (once created, the elements, order, and number of elements cannot be changed)
+var1 = 5
+my_tuple = ('str1', var1, 'str2', 3)
+my_tuple[0] # gets 'str1'
+my_tuple[1] # gets var1 which is 5
+one, two, three, four = my_tuple # unpacking a tuple (assigns each variable with each element of the tuple)
+my_one_element_tuple = (4,) # must have comma to be a tuple
 
 # Operators
 my_sum = 5 + 5
@@ -42,3 +52,87 @@ my_modulo = 5 % 5
 
 # Printing
 print("Hello, world")
+
+# Printing Strings and Numbers Together (no concatenation of numbers and strings automatically)
+str1 = "There are "
+num = 5
+str2 = " sheep."
+
+print(str1 + str(num) + str2) # note the use of the function str()
+
+
+
+# FUNCTIONS
+
+# W/ No Return
+def func_with_no_return(my_param):
+    print(my_param)
+
+func_with_no_return("Hi!")
+
+# W/ Return
+def add (num1, num2):
+	return num1 + num2
+
+sum = add(5, 6)
+
+# Multiple Returns
+def add_and_sub (num1, num2):
+	add = num1 + num2
+	sub = num1 - num2
+	return add, sub
+
+addNum, subNum = add_and_sub(10,5)
+
+# Keyword Arguments
+def func_using_keyword_args(param_one, param_two = "some value"):
+	print(param_one + param_two)
+
+func_using_keyword_args("Hi", "Hey") # will print "HiHey"
+func_using_keyword_args("Hi") # will print "Hisome value"
+func_using_keyword_args(param_one = "Hi", param_two = "Hey") # will print "HiHey"
+func_using_keyword_args("Hi", param_two = "Hey") # will print "HiHey"
+
+
+# CONTROL FLOW
+
+# If/Else/Else If
+my_num = 21
+
+if my_num == 10:
+	print("The num is 10!")
+elif my_num == 20:
+	print("The num is 20!")
+else:
+	print("The num is not 10 or 20!")
+
+# LOGICAL OPERATORS
+# and
+bool1 = (2 + 2 >= 4) and (-1 * -1 < 0) # False
+# or
+bool2 = (2 + 3 == 5) or (-1 + 1 == 2) # True
+# not
+bool3 = not(5 + 5 == 9) # True
+
+# LOOPS
+# For Loop
+myList = [1, 2, 3, 4, 5]
+for tempVar in myList:
+    print(tempVar)
+# For Loop Using range()
+for i in range(3):
+    print("Hi!")
+# While Loop
+index = 0
+while index < 5:
+    print(index)
+    index += 1
+
+# Break and Continue
+# just write break or continue where needed
+
+# Try and Except
+try:
+	# the code that might cause an error
+except ValueError: # the name of the error
+	print("Error!")
