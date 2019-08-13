73d15f2f80370fff72e2993bfb85450ea16c18bc
Kyla Yujiri <27860923+kylayujiri@users.noreply.github.com>
2019-08-13 09:48:23 -0700

    Initial commit

---
+++ pythonOverview.py
@@ -0,0 +1,44 @@
+# PYTHON
+# This document serves as a cheat sheet for what I've learned in Python.
+
+# This is how comments are written.
+
+# Primarily uses lower_case for variable names
+# Does not use brackets or semi-colons
+
+# Is dynamically typed (do not need to state variable types; the type of data an object can store is mutable)
+
+# Primitives
+my_integer = 5
+my_float = 5.0
+my_string = "5"
+my_bool = True #true and false are capitalized
+
+# More on Strings
+my_str = "Hi"
+my_str2 = "Kyla"
+my_str3 = my_str + " " + my_str2 # "Hi Kyla"
+
+make_nanana = "na" * 3 # Results in "nanana"
+
+# Slicing Strings
+orig_str = "Hello, world"
+sliced_str = orig_str[2:] # "llo, world"
+sliced_str2 = orig_str[0] + orig_str[11] # "Hd"
+
+# Multi Line Strings
+my_MLS = """ Hi!
+This is my Multi Line String (MLS)."""
+
+# Lists
+
+# Operators
+my_sum = 5 + 5
+my_difference = 5 - 5
+my_product = 5 * 5
+my_quotient = 5 / 5
+my_power = 5 ** 5 # This is 5^5
+my_modulo = 5 % 5
+
+# Printing
+print("Hello, world")
